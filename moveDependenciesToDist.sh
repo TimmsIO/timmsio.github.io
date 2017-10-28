80e106a53beeaff1f0f0216c6c78472643c6cf72
Morgan Timms <morgan@timms.io>
2017-10-29 10:58:49 +1100

    tools filter

--- moveDependenciesToDist.sh
+++ moveDependenciesToDist.sh
@@ -5,7 +5,7 @@ declare -A files
 # setup the move for multi.js files
 files["_excludes/multi.js/dist/multi.min.css"]="assets/css/multi/"
 files["_excludes/multi.js/dist/multi.min.js"]="assets/js/multi/"
-
+# files["_excludes/pushy-buttons/css/pushy-buttons.min.css"]="assets/css/pushyButtons/"
 
 for key in ${!files[@]}; do
     cp ${key} ${files[${key}]}
